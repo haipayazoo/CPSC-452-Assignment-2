b808fc5b8b31635c989eb648f0ee5f38c6d6879d
Christopher Bos <cbos95@csu.fullerton.edu>
2019-04-09 17:33:16 -0700

    added the base code

--- cipher.cpp
+++ cipher.cpp
@@ -5,6 +5,69 @@
 
 using namespace std;
 
+//enums for the cipher and mode arguments
+enum cipher {aes, des};
+enum mode {encrypt, decrypt};
+
+bool parseArguments(int argc,
+					char** argv,
+					enum cipher *argCipher,
+					unsigned char **argKey,
+					enum mode *argMode,
+					unsigned char **argInputFile,
+					unsigned char **argOutputFile)
+{
+	//check number of arguments
+	if(argc != 6)
+	{
+		printf("Incorrect number of arguments\n");
+		return false;
+	}
+
+	//parse the cipher
+	if(strcmp(argv[1], "AES") == 0)
+	{
+		*argCipher = aes;
+	}
+	else if(strcmp(argv[1], "DES") == 0)
+	{
+		*argCipher = des;
+	}
+	else
+	{
+		printf("Incorrect cipher [%s]\n", argv[1]);
+		return false;
+	}
+
+	//parse the key
+	*argKey = (unsigned char*)argv[2];
+
+	// TODO: validate key length (64 bits for des, 128 bits for aes)
+
+	//parse the mode
+	if(strcmp(argv[3], "ENC") == 0)
+	{
+		*argMode = encrypt;
+	}
+	else if(strcmp(argv[3], "DEC") == 0)
+	{
+		*argMode = decrypt;
+	}
+	else
+	{
+		printf("Incorrect mode [%s]\n", argv[3]);
+		return false;
+	}
+
+	//parse the input file
+	*argInputFile = (unsigned char*)argv[4];
+
+	//parse the output file
+	*argOutputFile = (unsigned char*)argv[5];
+
+	return true;
+}
+
 int main(int argc, char** argv)
 {
 	/**
@@ -15,12 +78,34 @@ int main(int argc, char** argv)
 	 * NOTE: due to the incomplete skeleton, the code may crash or
 	 * misbehave.
 	 */
-	
-	
-	
+
 	/* Create an instance of the DES cipher */	
 	CipherInterface* cipher = NULL; 
-		
+
+	//create variables for the cipher and mode
+	enum cipher argCipher;
+	unsigned char *argKey;
+	enum mode argMode;
+	unsigned char *argInputFile;
+	unsigned char *argOutputFile;
+
+	//parse arguments
+	if(!parseArguments(argc, argv, &argCipher, &argKey, &argMode, &argInputFile, &argOutputFile))
+	{
+		printf("Unable to parse arguments\n");
+		return 0;
+	}
+
+	//initialize the cipher
+	if(argCipher == aes)
+	{
+		cipher = new AES();
+	}
+	else if(argCipher == des)
+	{
+		cipher = new DES();
+	}
+
 	/* Error checks */
 	if(!cipher)
 	{
@@ -28,20 +113,24 @@ int main(int argc, char** argv)
 		__FILE__, __FUNCTION__, __LINE__);
 		exit(-1);
 	}
-	
+
 	/* Set the encryption key
 	 * A valid key comprises 16 hexidecimal
 	 * characters. Below is one example.
 	 * Your program should take input from
 	 * command line.
 	 */
-	cipher->setKey((unsigned char*)"0123456789abcdef");
+	cipher->setKey(argKey);
 	
 	/* Perform encryption */
-	//string cipherText = cipher->encrypt("hello world");
+	unsigned char* cipherText = cipher->encrypt((unsigned char*)"hello world");
+
+	printf("CT: %s\n", cipherText);
 	
 	/* Perform decryption */
-	//cipher->decrypt(cipherText);	
+	unsigned char* plainText = cipher->decrypt(cipherText);	
+
+	printf("PT: %s\n", plainText);
 	
 	return 0;
 }
