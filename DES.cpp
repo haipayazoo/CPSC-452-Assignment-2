6cc5849d0330a288a2670202a29fd340c8d13237
Cameron Mathos <haipayazoo@gmail.com>
2019-04-11 16:23:56 -0700

    Finished DES Encryption

--- DES.cpp
+++ DES.cpp
@@ -11,56 +11,56 @@ bool DES::setKey(const unsigned char* keyArray)
 	 * First let's covert the char string
 	 * into an integer byte string
 	 */
-	
-	
+
+
 	/* The key error code */
 	int keyErrorCode = -1;
 
 	/* A single byte */
-	unsigned char singleByte = 0;	
-	
+	unsigned char singleByte = 0;
+
 	/* The key index */
 	int keyIndex = 0;
-	
+
 	/* The DES key index */
 	int desKeyIndex = 0;
-		
+
 	/* Go through the entire key character by character */
 	while(desKeyIndex != 8)
 	{
 		/* Convert the key if the character is valid */
 		if((this->des_key[desKeyIndex] = twoCharToHexByte(keyArray + keyIndex)) == 'z')
 			return false;
-		
+
 		/* Go to the second pair of characters */
-		keyIndex += 2;	
-		
+		keyIndex += 2;
+
 		/* Increment the index */
 		++desKeyIndex;
 	}
-	
+
 	fprintf(stdout, "DES KEY: ");
-	
+
 	/* Print the key */
 	for(keyIndex = 0; keyIndex < 8; ++keyIndex)
 		fprintf(stdout, "%x", this->des_key[keyIndex]);
-	
-	fprintf(stdout, "\n");	
-	
-	
+
+	fprintf(stdout, "\n");
+
+
 	/* Set the encryption key */
 	if ((keyErrorCode = des_set_key_checked(&des_key, this->key)) != 0)
 	{
 		fprintf(stderr, "\nkey error %d\n", keyErrorCode);
-		
+
 		return false;
 	}
-	
-	/* All is well */	
+
+	/* All is well */
 	return true;
 }
 
-/**	
+/**
  * Encrypts a plaintext string
  * @param plaintext - the plaintext string
  * @return - the encrypted ciphertext string
@@ -69,17 +69,44 @@ unsigned char* DES::encrypt(const unsigned char* plaintext)
 {
 	//LOGIC:
 	//1. Check to make sure that the block is exactly 8 characters (i.e. 64 bits)
+	if(strlen((char*)plaintext) != 8) {
+		std::cout << "Plaintext is not 8 characters!\n";
+		return 0;
+	}
+
+	unsigned char text[8];
+	unsigned char encrypted_text[8];
+
+	for(int i = 0; i < 8; i++)
+		text[i] = plaintext[i];
+
 	//2. Declate an array DES_LONG block[2];
+	DES_LONG block[2];
+
 	//3. Use ctol() to convert the first 4 chars into long; store the result in block[0]
+	block[0] = ctol(text);
+
 	//4. Use ctol() to convert the second 4 chars into long; store the resul in block[1]
+	block[1] = ctol(text + 4);
+
 	//5. Perform des_encrypt1 in order to encrypt the block using this->key (see sample codes for details)
+	des_encrypt1(block, this->key, 1);
+
 	//6. Convert the first ciphertext long to 4 characters using ltoc()
+	ltoc(block[0], encrypted_text);
+
 	//7. Convert the second ciphertext long to 4 characters using ltoc()
-	//8. Save the results in the the dynamically allocated char array 
-	// (e.g. unsigned char* bytes = nerw unsigned char[8]).
+	ltoc(block[1], encrypted_text + 4);
+
+	//8. Save the results in the the dynamically allocated char array
+	unsigned char* ciphertext = new unsigned char[8];
+
+	for(int i = 0; i < 8; i++)
+		ciphertext[i] = encrypted_text[i];
+
 	//9. Return the pointer to the dynamically allocated array.
-	
-	return NULL;
+
+	return ciphertext;
 }
 
 /**
@@ -100,11 +127,11 @@ unsigned char* DES::decrypt(const unsigned char* ciphertext)
  * @return - the long integer (32 bits) where each byte
  * is equivalent to one of the bytes in a character array
  */
-DES_LONG DES::ctol(unsigned char *c) 
+DES_LONG DES::ctol(unsigned char *c)
 {
         /* The long integer */
 	DES_LONG l;
-        
+
 	l =((DES_LONG)(*((c)++)));
         l = l | (((DES_LONG)(*((c)++)))<<8L);
         l = l | (((DES_LONG)(*((c)++)))<<16L);
@@ -113,13 +140,13 @@ DES_LONG DES::ctol(unsigned char *c)
 };
 
 
-/** 
+/**
  * Converts a long integer (4 bytes = 32 bits)
  * into an array of 8 characters.
  * @param l - the long integer to convert
  * @param c - the character array to store the result
  */
-void DES::ltoc(DES_LONG l, unsigned char *c) 
+void DES::ltoc(DES_LONG l, unsigned char *c)
 {
         *((c)++)=(unsigned char)(l&0xff);
         *((c)++)=(unsigned char)(((l)>> 8L)&0xff);
@@ -134,14 +161,14 @@ void DES::ltoc(DES_LONG l, unsigned char *c)
  */
 unsigned char DES::charToHex(const char& character)
 {
-	/* Is the first digit 0-9 ? */	
-	if(character >= '0' && character <= '9')	
+	/* Is the first digit 0-9 ? */
+	if(character >= '0' && character <= '9')
 		/* Convert the character to hex */
 		return character - '0';
 	/* It the first digit a letter 'a' - 'f'? */
 	else if(character >= 'a' && character <= 'f')
 		/* Conver the cgaracter to hex */
-		return (character - 97) + 10;	
+		return (character - 97) + 10;
 	/* Invalid character */
 	else return 'z';
 }
@@ -161,30 +188,28 @@ unsigned char DES::twoCharToHexByte(const unsigned char* twoChars)
 {
 	/* The byte */
 	unsigned char singleByte;
-	
+
 	/* The second character */
 	unsigned char secondChar;
 
 	/* Convert the first character */
-	if((singleByte = charToHex(twoChars[0])) == 'z') 
+	if((singleByte = charToHex(twoChars[0])) == 'z')
 	{
 		/* Invalid digit */
 		return 'z';
 	}
-	
+
 	/* Move the newly inserted nibble from the
 	 * lower to upper nibble.
 	 */
 	singleByte = (singleByte << 4);
-	
+
 	/* Conver the second character */
 	if((secondChar = charToHex(twoChars[1])) == 'z')
-		return 'z'; 
-	
-	/* Insert the second value into the lower nibble */	
+		return 'z';
+
+	/* Insert the second value into the lower nibble */
 	singleByte |= secondChar;
 
 	return singleByte;
 }
-
-
